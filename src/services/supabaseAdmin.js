825f95cd29b6a37196de7e4097ec0b990d659464
takieeldeen <takie.eldeen1998@gmail.com>
2024-03-25 11:41:25 +0200

    Initial Commit

--- src/services/supabaseAdmin.js
+++ src/services/supabaseAdmin.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const key = process.DB_SERVICE_ROLL_KEY;
-const url = process.DB_URL;
+const key = process.env.DB_SERVICE_ROLL_KEY;
+const url = process.env.DB_URL;
 
 const supabaseAdmin = createClient(url, key);
 export default supabaseAdmin;
